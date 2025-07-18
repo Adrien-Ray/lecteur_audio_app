5543eaa9323a0114ffefedaab77855a1ed9b15f1
Adrien-Ray <forpub@accesdenied.net>
2025-07-18 20:32:26 +0200

    fix system of checked with class of brother, now is only based on checked checkbox #16

--- assets/scripts/newCall.js
+++ assets/scripts/newCall.js
@@ -1,21 +1,37 @@
 export function newCall(
-    itemDOMcssSelector = 'ul#listFiles > li > span',
+    checkedRequired = false,
     isRandomMode = false
 ) {
-    let itemDOM = document.querySelectorAll(itemDOMcssSelector);
+    let liDOM = document.querySelectorAll('#listFiles > li'); // return nodeList checkboxes
+    console.log('liDOM',liDOM);
+    let liIndexTrigger = [];
+    let actualIndex = false;
+    for (let i = 0; i < liDOM.length; i++) {
+        const liDOMelement = liDOM[i];
+        console.log(liDOMelement);
+        if (liDOMelement.childNodes[1].classList.contains('current')) {
+            actualIndex = i;
+        }
+        if (checkedRequired) {
+            if (liDOMelement.childNodes[0].checked) {
+                liIndexTrigger.push(i);
+            }
+        } else {
+            liIndexTrigger.push(i);
+        }
+    }
     if (isRandomMode) {
-        let randomIndex = Math.floor(Math.random() * itemDOM.length);
-        let randomItem = itemDOM[randomIndex];
+        let randomIndex = Math.floor(Math.random() * liIndexTrigger.length);
+        let randomIndexValue = liIndexTrigger[randomIndex];
+        let randomItem = liDOM[randomIndexValue].childNodes[1];
         randomItem.click();
     } else {
-        let arrayLength = itemDOM.length;
-        let itemDOMArray = Array.from(itemDOM);
-        let actualIndex = itemDOMArray.findIndex(item => item.classList.contains('current'));
-        let nextIndex = actualIndex+1;
-        if(nextIndex >= arrayLength) {
-            nextIndex = 0;
+        let nextIndex;
+        nextIndex = liIndexTrigger.find((nextVal) => nextVal > actualIndex);
+        if(!nextIndex) {
+            nextIndex = liIndexTrigger[0];
         };
-        let nextItem = itemDOM[nextIndex];
+        let nextItem = liDOM[nextIndex].childNodes[1];
         setTimeout(() => {
             nextItem.click();
         }, 100);
