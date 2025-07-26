7c5af4f433e8897ff82168ffa89a9f943914f40b
Adrien-Ray <forpub@accesdenied.net>
2025-07-26 12:10:43 +0200

    Profiles create, get in localstorage and DOM #12

--- assets/script.js
+++ assets/script.js
@@ -1,41 +1,13 @@
 import { constructList } from "./scripts/constructList.js";
+import { constructListProfiles } from "./scripts/constructListProfiles.js";
+import { getListProfileInLocalStorage } from "./scripts/getListProfileInLocalStorage.js";
 import { menuSpecial } from "./scripts/menuSpecial.js";
 import { lecteurEnded } from "./scripts/lecteurEnded.js";
 import { getListFilesInLocalStorage } from "./scripts/getListFilesInLocalStorage.js";
+import { addFileListener } from "./scripts/addFileListener.js";
 
-/*const { dialog } = require('electron');
-const fs = require('fs');*/
-
-function addFileListener() {
-    document.getElementById('addFileButton').addEventListener('click', async () => {
-        const files = await window.electronAPI.openFiles();
-        console.log('Fichiers sélectionnés :', files);
-        let initialListFiles = getListFilesInLocalStorage();
-        let newInListFiles = []
-        files.forEach(filePath => {
-            if (filePath.endsWith('.mp3')) {
-                newInListFiles.push({
-                    path: filePath.substring(0, filePath.lastIndexOf('/')),
-                    name: filePath.substring(filePath.lastIndexOf('/') + 1),
-                    uuid: `${Math.floor(Math.random() * 9)}${Math.floor(Math.random() * 9)}${Math.floor(Math.random() * 9)}${Math.floor(Math.random() * 9)}-${Math.floor(Math.random() * 9)}${Math.floor(Math.random() * 9)}${Math.floor(Math.random() * 9)}${Math.floor(Math.random() * 9)}-${Math.floor(Math.random() * 9)}${Math.floor(Math.random() * 9)}${Math.floor(Math.random() * 9)}${Math.floor(Math.random() * 9)}-${Math.floor(Math.random() * 9)}${Math.floor(Math.random() * 9)}${Math.floor(Math.random() * 9)}${Math.floor(Math.random() * 9)}-${Math.floor(Math.random() * 9)}${Math.floor(Math.random() * 9)}${Math.floor(Math.random() * 9)}${Math.floor(Math.random() * 9)}`
-                })
-            } else {
-                console.error(`${filePath} n'est pas un mp3`);
-            }
-        });
-        constructList(newInListFiles);
-        newInListFiles.forEach(element => {
-            initialListFiles.push(element);
-        });
-        localStorage.setItem('listFiles', JSON.stringify(initialListFiles));
-        // location.reload();
-    });
-}
-
-let listFiles = getListFilesInLocalStorage();
-console.log('listFiles', listFiles);
-
-constructList(listFiles);
+constructList(getListFilesInLocalStorage());
+constructListProfiles(getListProfileInLocalStorage());
 menuSpecial();
 lecteurEnded();
 addFileListener();
---
+++ assets/scripts/generateUuid.js
@@ -0,0 +1,3 @@
+export function generateUuid() {
+    return `${Math.floor(Math.random() * 9)}${Math.floor(Math.random() * 9)}${Math.floor(Math.random() * 9)}${Math.floor(Math.random() * 9)}-${Math.floor(Math.random() * 9)}${Math.floor(Math.random() * 9)}${Math.floor(Math.random() * 9)}${Math.floor(Math.random() * 9)}-${Math.floor(Math.random() * 9)}${Math.floor(Math.random() * 9)}${Math.floor(Math.random() * 9)}${Math.floor(Math.random() * 9)}-${Math.floor(Math.random() * 9)}${Math.floor(Math.random() * 9)}${Math.floor(Math.random() * 9)}${Math.floor(Math.random() * 9)}-${Math.floor(Math.random() * 9)}${Math.floor(Math.random() * 9)}${Math.floor(Math.random() * 9)}${Math.floor(Math.random() * 9)}`;
+}
